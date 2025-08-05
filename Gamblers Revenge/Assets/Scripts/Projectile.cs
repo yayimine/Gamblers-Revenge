333493652d60fcd3a02cf6d14298ad8fe69566cd
Krt135 <155903803+Krt135@users.noreply.github.com>
2025-08-05 13:41:58 -0400

    fixed slash to be no pierce and special slash to be infinite pierce

--- Gamblers Revenge/Assets/Scripts/Projectile.cs
+++ Gamblers Revenge/Assets/Scripts/Projectile.cs
@@ -24,24 +24,25 @@ public class Projectile : MonoBehaviour
         transform.position = new Vector3(transform.position.x, //take the player's x position
 transform.position.y, //take the player's y position
 PlayerController.instance.transform.position.z); //keep the original z position for the camera
-        print("spawned");
     }
     
     void OnTriggerEnter2D(Collider2D other)
     {
-        print("hit");
-        if (_piercesLeft <= 0) return;
 
         // only hit things tagged "Enemy"
-        if (!other.CompareTag("Enemy") && !other.CompareTag("Player")) return;
+        if (!other.CompareTag("Enemy")) return;
         if (_alreadyHit.Contains(other)) return;
 
         _alreadyHit.Add(other);
-        _piercesLeft--;
 
         // destroy when out of pierces
-        if (_piercesLeft <= 0)
+        if (_piercesLeft == 0)
+        {
             Destroy(gameObject);
+        } else
+        {
+            _piercesLeft--;
+        }
     }
 }
 
