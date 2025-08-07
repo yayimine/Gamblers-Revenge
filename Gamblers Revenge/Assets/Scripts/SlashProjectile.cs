6115349e972b5e2dd76479f35053a167c0f90135
Krt135 <155903803+Krt135@users.noreply.github.com>
2025-08-07 15:42:43 -0400

    added dashing

---
+++ Gamblers Revenge/Assets/Scripts/SlashProjectile.cs
@@ -0,0 +1,54 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slash : MonoBehaviour
+{
+    // make these public so other scripts (like Sword) can assign to them
+    [HideInInspector] public float damage;
+    [HideInInspector] public int maxPierces;
+
+    [HideInInspector] public float initialSpeed = 20f;
+    public float lifetime = 3f;
+
+    private HashSet<Collider2D> _alreadyHit = new HashSet<Collider2D>();
+    private int _piercesLeft;
+
+    void Awake()
+    {
+        // if nobody set them externally, fall back to the player's defaults
+        if (damage <= 0f)
+            damage = PlayerController.instance.damage;
+
+        if (maxPierces <= 0)
+            maxPierces = PlayerController.instance.maxPierces;
+
+        _piercesLeft = maxPierces;
+        initialSpeed = GetComponent<Rigidbody2D>().velocity.magnitude;
+    }
+
+    void Start()
+    {
+        Destroy(gameObject, lifetime);
+
+        // keep the same z-depth as the player (if you really need it)
+        Vector3 pos = transform.position;
+        pos.z = PlayerController.instance.transform.position.z;
+        transform.position = pos;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.CompareTag("Enemy") || _alreadyHit.Contains(other))
+            return;
+
+        _alreadyHit.Add(other);
+
+        // apply `damage` to the enemy here, e.g.:
+        // other.GetComponent<Enemy>()?.TakeDamage(damage);
+
+        if (_piercesLeft <= 0)
+            Destroy(gameObject);
+        else
+            _piercesLeft--;
+    }
+}
